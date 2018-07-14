9deedbfcc568394fdc11e417676d69313b7a2f9c
maluu <souzamalu2016@gmail.com>
2018-07-14 13:12:32 -0300

    Add files via upload

---
+++ calculo.php
@@ -0,0 +1,13 @@
+<?php
+
+$primeiratrinca1 = $_GET['primeiratrinca1'];
+$segundatrinca2 = $_GET['segundatrinca2'];
+$terceiratrinca3 = $_GET['terceiratrinca3'];
+$quartatrinca4 = $_GET['quartatrinca4'];
+
+echo
+   "Você digitou o IP:";
+echo $primeiratrinca1;
+echo $segundatrinca2;
+echo $terceiratrinca3;
+echo $quartatrinca4;
